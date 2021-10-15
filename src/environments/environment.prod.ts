fb5035ca8c3ada7257dee5c9cb30d731e6dd1a5b
claudiahW <murungiclaud@gmail.com>
2021-10-15 12:02:54 +0300

    add new access token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:"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"
+  apiUrl:"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"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 
 export const environment = {
   production: false,
-  apiUrl:"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"
+  apiUrl:"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"
 
 };
 
